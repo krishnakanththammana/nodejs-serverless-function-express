e1073f8a339c48e8c929fe72af11ef8c95d5166a
krishnakanththammana <krishnakanththammana@gmail.com>
2023-11-01 20:21:33 +0530

    trying to get vercel running

--- api/crew.ts
+++ api/crew.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.json({
     columns: [
       {
